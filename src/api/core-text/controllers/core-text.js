5ad8c035aa37b23d8ed2c96a2a264eab737e934e
Manvendra2000 <mzealpro@gmail.com>
2026-02-10 14:53:18 +0530

    first commit

---
+++ src/api/core-text/controllers/core-text.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * core-text controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::core-text.core-text');
---
+++ src/api/core-text/routes/core-text.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * core-text router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::core-text.core-text');
