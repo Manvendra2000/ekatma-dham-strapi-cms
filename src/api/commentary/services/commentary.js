848f00a064ffc696a7c515ae01f5b8ea356ce0ca
Manvendra2000 <mzealpro@gmail.com>
2026-02-12 14:44:53 +0530

    clean slate

--- src/api/commentary/services/commentary.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * commentary service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::commentary.commentary');
